727eb28b10470c3bd079cbc5cd0bf3a8f812e833
Fathur <fathur260605@gmail.com>
2024-11-06 09:46:15 +0700

    initial commit

---
+++ lib/menu.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String npm = '2306206282'; // NPM
+  final String name = 'Fathurrahman Kesuma Ridwan'; // Nama
+  final String className = 'PBP E'; // Kelas
+
+  final List<ItemHomepage> items = [
+    ItemHomepage("Lihat Daftar Produk", Icons.inventory),
+    ItemHomepage("Tambah Produk", Icons.add),
+    ItemHomepage("Logout", Icons.logout),
+  ];
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
+    return Scaffold(
+      // AppBar dengan warna hijau.
+      appBar: AppBar(
+        title: const Text(
+          'Toko-ya',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.green, // Set navbar color to green.
+      ),
+      // Body halaman dengan padding di sekelilingnya.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Welcome to Toko-ya!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  GridView.count(
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    children: items.asMap().entries.map((entry) {
+                      int index = entry.key;
+                      ItemHomepage item = entry.value;
+                      // Apply different green hues to each button based on index.
+                      return ItemCard(item, buttonColor: _getButtonColor(index));
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Function to get different shades of green based on index.
+  Color _getButtonColor(int index) {
+    switch (index) {
+      case 0:
+        return Colors.green[700]!;
+      case 1:
+        return Colors.green[500]!;
+      case 2:
+        return Colors.green[300]!;
+      default:
+        return Colors.green;
+    }
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2.0,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 3.5,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHomepage {
+  final String name;
+  final IconData icon;
+
+  ItemHomepage(this.name, this.icon);
+}
+
+class ItemCard extends StatelessWidget {
+  final ItemHomepage item;
+  final Color buttonColor;
+
+  const ItemCard(this.item, {super.key, required this.buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: buttonColor,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+                SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
+            );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
